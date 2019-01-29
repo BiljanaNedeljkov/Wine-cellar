2136316f96cae080c80c616836f7b43a342b3f41
Biljana Nedeljkov <biljanapezos@yahoo.com>
2019-01-29 01:25:51 +0100

    Zavrsena navigacija

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { WineListComponent } from './wine/wine-list/wine-list.component';
@@ -14,7 +14,8 @@ import { EditWineComponent } from './wine/edit-wine/edit-wine.component';
     EditWineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
