585507599aa935a31f46f1890dded24339739dac
Biljana Nedeljkov <biljanapezos@yahoo.com>
2019-01-29 01:46:01 +0100

    Zavrseno rutiranje

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,31 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/navbar/navbar.component';
 import { WineListComponent } from './wine/wine-list/wine-list.component';
 import { EditWineComponent } from './wine/edit-wine/edit-wine.component';
+import { HomeComponent } from './wine/home/home.component';
+import { AboutComponent } from './wine/about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'wines', component: WineListComponent},
+  {path: '', redirectTo: 'wines', pathMatch: "full"}
+
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     WineListComponent,
-    EditWineComponent
+    EditWineComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
